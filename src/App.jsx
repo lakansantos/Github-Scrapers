fe136c47807502fdd6d1ad94bdaa931bfc741bc5
Lakan Santos <rizesuoracle25@gmail.com>
2023-02-02 15:52:21 +0800

    added more design

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import './App.css'
 import Search from './components/Search'
+import {FiExternalLink} from 'react-icons/fi'
 
 
 
@@ -58,21 +59,26 @@ function App() {
           <Search handleSubmit={handleSubmit} inputValue={inputValue} setInputValue={inputValue => setInputValue(inputValue)}/>
         </div>
 
-        <div className='flex flex-row items-center justify-center mt-12 h-fit'>
-          <div className="img-container">
-            <img src={data.avatar_url} alt="avatar" className='w-72 h-72 object-contain'/>
-          </div>
-
-          <div className="content-container text-left ml-4">
-            <h1><span>Name:</span> {data.name}</h1>
-            <div className='h-fit text-left'>
-              <p><span>Bio:</span> {ifEmpty(data.bio)}</p>
-              <p><span>Number of repositories:</span> {data.public_repos}</p>
-              <p><span>Followers:</span> {data.followers}</p>
-              <p><span>Following:</span> {data.following}</p>
-              <p><span>Location:</span> {ifEmpty(data.location)}</p>
-              <p><span>Company:</span> {ifEmpty(data.company)}</p>
+        <div className='flex flex-row items-center justify-center mt-12 h-1/2   w-full'>
+          <div className='flex items-center justify-center h-fit w-3/4 h-full'>
+            <div className="img-container h-full w-50 flex justify-center bg-sky-400">
+              <img src={data.avatar_url} alt="avatar" className='w-72 object-fixed'/>
             </div>
+      
+            <div className="content-container pl-4 pt-4 text-left w-3/6 bg-gray-200">
+              <h1 className='name text-4xl'>{data.name}</h1>
+              <div className='h-fit text-left mt-4'>
+                <p><span>Bio:</span> {ifEmpty(data.bio)}</p>
+                <p><span>Number of repositories:</span> {data.public_repos}</p>
+                <p><span>Followers:</span> {data.followers}</p>
+                <p><span>Following:</span> {data.following}</p>
+                <p><span>Location:</span> {ifEmpty(data.location)}</p>
+                <p><span>Company:</span> {ifEmpty(data.company)}</p>
+              </div>
+              <a href={`https://github.com/${data.login}`} target="_blank">
+                <button className='bg-sky-500 w-fit p-1  gap-2 text-white flex items-center justify-center mt-4'>Visit<FiExternalLink/></button>
+              </a>
+            </div>              
           </div>
         </div>
       </div>
